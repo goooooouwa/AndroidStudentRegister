7b74a5df30f9ed4f2619f6675bf4089d56d8f92d
Shunfa Xu <gouwa5700@gmail.com>
2024-10-12 18:40:11 -0700

    save student and clear input

--- app/src/main/java/com/example/studentregister/MainActivity.kt
+++ app/src/main/java/com/example/studentregister/MainActivity.kt
@@ -1,20 +1,66 @@
 package com.example.studentregister
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.studentregister.db.Student
+import com.example.studentregister.db.StudentDatabase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var nameEditText: EditText
+    private lateinit var emailEditText: EditText
+    private lateinit var saveButton: Button
+    private lateinit var clearButton: Button
+
+    private lateinit var viewModel: StudentViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        nameEditText = findViewById(R.id.etName)
+        emailEditText = findViewById(R.id.etEmail)
+        saveButton = findViewById(R.id.btnSave)
+        clearButton = findViewById(R.id.btnClear)
+
+        val dao = StudentDatabase.getInstance(application).studentDao()
+        val factory = StudentViewModelFactory(dao)
+        viewModel = ViewModelProvider(this, factory).get(StudentViewModel::class.java)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        saveButton.setOnClickListener {
+            saveStudentData()
+            clearInput()
+        }
+
+        clearButton.setOnClickListener {
+            clearInput()
+        }
+    }
+
+    private fun saveStudentData() {
+        viewModel.insertStudent(
+            Student(
+                0,
+                nameEditText.text.toString(),
+                emailEditText.text.toString()
+            )
+        )
+    }
+
+    private fun clearInput(){
+        nameEditText.setText("")
+        emailEditText.setText("")
     }
 }
